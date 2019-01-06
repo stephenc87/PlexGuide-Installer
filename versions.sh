df3ecbef60f519814916fe85f99ef1a92c4ba4fb
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-01-06 02:40:44 -0500

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -1,10 +1,10 @@
-8.0.8
+8.0.9
 v8
 ########################### VERSION BELOW ARE PRIOR
+8.0.8
 8.0.7
 8.0.6
 8.0.5
 8.0.4
-8.0.1
 ########################### COMMAND BELOW IS FOR INTERNAL TESTING
 next
