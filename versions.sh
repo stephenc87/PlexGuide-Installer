626fb94ad817d2fc0a802e9f33289bec2fc9ef31
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-01-28 18:28:05 -0500

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -2,10 +2,9 @@
 8.3.2
 ###########################-VERSIONS-PRIOR
 8.2.5
-###### DONT RECOMMEND BELOW
+###### DONT RECOMMEND BELOW (NO LONGER UPDATED)
 8.1.8
 ###########################-COMMANDS-BELOW-FOR-TESTING
-v8.1base
 v8.2base
 v8.3base
 next
