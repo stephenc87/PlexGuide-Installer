036518c9dc1ca8dec1e4b10f820e4b74bacbb4a5
Robert Baker <se7enalive@gmail.com>
2019-02-11 17:50:53 -0700

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -4,11 +4,6 @@ v8.4base
 8.4.5
 ###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
 8.3.17
-8.3.16
-8.3.15
-8.3.13
-8.3.7
-8.2.6
 ###########################-COMMANDS-BELOW-FOR-TESTING
 v8.3base
 next
