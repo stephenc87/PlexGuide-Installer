340a53ed6d800e396842de77d7b9f32e63a04b6e
Robert Baker <se7enalive@gmail.com>
2019-02-15 19:44:43 -0700

    bump

--- versions.sh
+++ versions.sh
@@ -1,8 +1,8 @@
-8.4.7
+8.4.8
 v8.4base
 ###########################-VERSIONS-PRIOR
+8.4.7
 8.4.6
-8.4.5
 ###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
 8.3.17
 ###########################-COMMANDS-BELOW-FOR-TESTING
